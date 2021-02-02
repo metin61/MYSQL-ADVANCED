3fc717b6ae773cf0c61293894b252919ccbf8bb1
metin celik <metin.celik@student.rocva.nl>
2021-02-02 14:26:47 +0100

    antwoorden

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,14 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) VALUES
+ 
+("1", 'Henk', 'jan', "0D", "Henksneevleitweg", 1069, 2002-03-25, "Amsterdam"),
+("2", 'piet', 'jannes', "1D", "Henksneevleit", 1061, 2001-03-25, "Amsterdam"),
+("3", 'mo', 'janos', "2D", "Henksnee", 1062, 2006-03-25, "Amsterdam"),
+("4", 'ap', 'janas', "3D", "Henks", 1068, 2008-03-25, "Amsterdam"),
+("5", 'meto', 'janus', "4D", "Henkstraat", 1066, 2004-03-25, "Amsterdam"),
+("6", 'jks', 'janis', "5D", "Henksdas", 1064, 2009-03-25, "Amsterdam")
+
+2
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats) 
+ 
+VALUES(99, 'Henk', 'jan', "0D", "Henksneevleitweg", 1069, 2002-03-25, "Amsterdam");
 
--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,6 @@
 
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats) VALUES
+ 
+("Hedsank", 'jan', 2002-03-25, "Amsterdam"),
+("pidset", 'jannes', 2001-03-25, "Amsterdam"),
+("mdso", 'janos',  2006-03-25, "Amsterdam")

5ef3ecaa5339a9f972bbe3601e3ec1fc9e0f6059
metin celik <metin.celik@student.rocva.nl>
2021-02-02 14:58:59 +0100

    update antw

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,20 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET woonplaats = "Amstelveen" 
 -- Opdracht 2 
+UPDATE studenten2
+SET adres = "Amstelveenseweg", postcode= 1098
+WHERE student_id IN (8,9,10);
 
 -- Opdracht 3
-
+UPDATE studenten2
+SET geboortedatum = 2000-12-12
+WHERE student_id = 2;
 -- Opdracht 4
-
+UPDATE studenten2
+SET klas = "9A", klas = "9C"
+WHERE student_id IN (8,4);
 -- Opdracht 5
+UPDATE studenten2
+SET voornaam = "Johannah"
+WHERE student_id IN (6);

bb105db4abaf1eb4d78f1a1caa5496d125b5498e
metin celik <metin.celik@student.rocva.nl>
2021-02-03 14:03:06 +0100

    update

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -8,6 +8,11 @@ UPDATE studenten2
 SET klas="9Z"
 WHERE student_id >7;
 -- Opdracht 3
-
+UPDATE studenten2
+SET woonplaats="Amsterdan"
+WHERE student_id <=2 OR ">7"; 
 -- Opdracht 4
+UPDATE studenten2
+SET woonplaats="Uithoorn"
+WHERE klas="9X" AND woonplaats= "Amsterdam";
 
